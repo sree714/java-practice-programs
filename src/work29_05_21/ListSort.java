8e8be783826e280ad9b5a7c965be477f8641fb24
Smrity <smritysreebanerjee714@gmail.com>
2021-05-29 12:52:20 +0530

    Arraylist sorting

---
+++ src/work29_05_21/ListSort.java
@@ -0,0 +1,38 @@
+package work29_05_21;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class ListSort {
+    public static void main(String[] args) {
+        //float total=0;
+       // float m,e;
+        ArrayList<StudentSort> studentList = new ArrayList<>();
+        studentList.add(new StudentSort("Smrity",7,100,90));
+        studentList.add(new StudentSort("Soumo",4,50,65));
+        studentList.add(new StudentSort("Mohar",2,100,89));
+        studentList.add(new StudentSort("Nayan",5,56,67));
+        studentList.add(new StudentSort("prity",6,34,45));
+
+
+        studentList.sort(Comparator.comparing(StudentSort::getTotal));
+        //Collections.reverse(studentList);
+        System.out.println("ArrayList After Sorting:");
+
+        for (int i=0;i<studentList.size();i++){
+
+          //  m=(studentList.get(i).getMathScore());
+            //e=(studentList.get(i).getEngScore());
+            //total=m+e;
+
+            print("Name:"+studentList.get(i).getName());
+            print("roll:"+studentList.get(i).getRoll());
+            print("Result= "+studentList.get(i).getTotal());
+
+        }
+
+    }
+
+    private static void print(String str) {
+        System.out.println(str);
+    }
+}
---
+++ src/work29_05_21/StudentSort.java
@@ -0,0 +1,35 @@
+package work29_05_21;
+
+public class StudentSort {
+    private String name;
+    private int roll;
+    private float mathScore;
+    private float engScore;
+    private float total;
+
+    public StudentSort(String name, int roll, float mathScore,float engScore) {
+        this.name = name;
+        this.roll = roll;
+        this.mathScore = mathScore;
+        this.engScore=engScore;
+        this.total=mathScore+engScore;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public float getMathScore() {
+        return mathScore;
+    }
+    public float getEngScore(){
+        return engScore;
+    }
+    public float getTotal()
+    {
+        return total;
+    }
+}
