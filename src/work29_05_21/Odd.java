0be9aee5c66b232451f9d052a0f9198ed0daa004
root <soumojyotiganguly270@gmail.com>
2021-05-29 20:15:32 +0530

    fix line work29_05_21

--- src/work29_05_21/Odd.java
+++ src/work29_05_21/Odd.java
@@ -1,29 +1,27 @@
 package work29_05_21;
+
 import java.util.*;
-public class Odd
- {
 
-        public static void main(String[] args)
-        {
-            int n;
-            System.out.println("how many elemets?");
-            Scanner sc = new Scanner(System.in);
-            n = sc.nextInt();
-            int[]arr = new int[n];
-            System.out.println("enter the elements:");
-            for(int i = 0; i < n; i++)
-            {
-                arr[i] = sc.nextInt();
-            }
-            int count_odd =0;
-            for(int i = 0; i < n; i++)
-            {
-                if(arr[i] % 2 != 0)
-                    count_odd++;
-            }
-            System.out.println("No of odd elements:"+count_odd);
+public class Odd {
 
-
+    public static void main(String[] args) {
+        int n;
+        System.out.println("how many elemets?");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
- }
+        int count_odd = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0)
+                count_odd++;
+        }
+        System.out.println("No of odd elements:" + count_odd);
+
+
+    }
+}
 
