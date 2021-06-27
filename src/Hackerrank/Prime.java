3c9d48828599e31d61a9389201a7d4570a708f23
sree714 <smritysreebanerjee714@gmail.com>
2021-06-27 17:03:40 +0530

    Hackerrank Day 25:   Running Time and Complexity

---
+++ src/Hackerrank/Prime.java
@@ -0,0 +1,41 @@
+package Hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            int n = scan.nextInt();
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
+        }
+        scan.close();
+    }
+
+    private static boolean isPrime(int n) {
+        if (n < 2)
+        {
+            return false;
+        }
+        else if (n == 2)
+        {
+            return true;
+        }
+        else if (n % 2 == 0)
+        {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
